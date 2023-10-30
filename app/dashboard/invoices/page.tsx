ca184017f0e214e6659302a4fa11765c8a1a78db
Julian Bustos <julian.m.bustos@gmail.com>
2023-10-30 18:48:26 -0400

    ads basic app

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <p>
+      Invoices page
+    </p>
+  );
+}
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+  return (
+    <main>
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
+
+export default Page;
